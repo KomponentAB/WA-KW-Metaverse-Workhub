9ce7fe5ee03a7d26b35a68a23c87013bb3535d4a
Klinshy <79143155+Klinshy@users.noreply.github.com>
2024-05-22 11:05:02 +0200

    initial push

---
+++ CWT Tileset.tsx
@@ -0,0 +1,132 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="CWT Tileset" tilewidth="32" tileheight="32" tilecount="10300" columns="100">
+ <image source="CWT Tileset.png" width="3200" height="3296"/>
+ <tile id="1711">
+  <animation>
+   <frame tileid="1711" duration="200"/>
+   <frame tileid="1911" duration="200"/>
+   <frame tileid="2111" duration="200"/>
+   <frame tileid="2311" duration="200"/>
+   <frame tileid="2511" duration="200"/>
+   <frame tileid="2311" duration="200"/>
+   <frame tileid="2111" duration="200"/>
+   <frame tileid="1911" duration="200"/>
+   <frame tileid="1711" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1712">
+  <animation>
+   <frame tileid="1712" duration="200"/>
+   <frame tileid="1912" duration="200"/>
+   <frame tileid="2112" duration="200"/>
+   <frame tileid="2312" duration="200"/>
+   <frame tileid="2512" duration="200"/>
+   <frame tileid="2312" duration="200"/>
+   <frame tileid="2112" duration="200"/>
+   <frame tileid="1912" duration="200"/>
+   <frame tileid="1712" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1713">
+  <animation>
+   <frame tileid="1713" duration="200"/>
+   <frame tileid="1913" duration="200"/>
+   <frame tileid="2113" duration="200"/>
+   <frame tileid="2313" duration="200"/>
+   <frame tileid="2513" duration="200"/>
+   <frame tileid="2313" duration="200"/>
+   <frame tileid="2113" duration="200"/>
+   <frame tileid="1913" duration="200"/>
+   <frame tileid="1713" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1714">
+  <animation>
+   <frame tileid="1714" duration="200"/>
+   <frame tileid="1914" duration="200"/>
+   <frame tileid="2114" duration="200"/>
+   <frame tileid="2314" duration="200"/>
+   <frame tileid="2514" duration="200"/>
+   <frame tileid="2314" duration="200"/>
+   <frame tileid="2114" duration="200"/>
+   <frame tileid="1914" duration="200"/>
+   <frame tileid="1714" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1811">
+  <animation>
+   <frame tileid="1811" duration="200"/>
+   <frame tileid="2011" duration="200"/>
+   <frame tileid="2211" duration="200"/>
+   <frame tileid="2411" duration="200"/>
+   <frame tileid="2611" duration="200"/>
+   <frame tileid="2411" duration="200"/>
+   <frame tileid="2211" duration="200"/>
+   <frame tileid="2011" duration="200"/>
+   <frame tileid="1811" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1812">
+  <animation>
+   <frame tileid="1812" duration="200"/>
+   <frame tileid="2012" duration="200"/>
+   <frame tileid="2212" duration="200"/>
+   <frame tileid="2412" duration="200"/>
+   <frame tileid="2612" duration="200"/>
+   <frame tileid="2412" duration="200"/>
+   <frame tileid="2212" duration="200"/>
+   <frame tileid="2012" duration="200"/>
+   <frame tileid="1812" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1813">
+  <animation>
+   <frame tileid="1813" duration="200"/>
+   <frame tileid="2013" duration="200"/>
+   <frame tileid="2213" duration="200"/>
+   <frame tileid="2413" duration="200"/>
+   <frame tileid="2613" duration="200"/>
+   <frame tileid="2413" duration="200"/>
+   <frame tileid="2213" duration="200"/>
+   <frame tileid="2013" duration="200"/>
+   <frame tileid="1813" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="1814">
+  <animation>
+   <frame tileid="1814" duration="200"/>
+   <frame tileid="2014" duration="200"/>
+   <frame tileid="2214" duration="200"/>
+   <frame tileid="2414" duration="200"/>
+   <frame tileid="2614" duration="200"/>
+   <frame tileid="2414" duration="200"/>
+   <frame tileid="2214" duration="200"/>
+   <frame tileid="2014" duration="200"/>
+   <frame tileid="1814" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="2116">
+  <animation>
+   <frame tileid="2116" duration="200"/>
+   <frame tileid="2117" duration="200"/>
+   <frame tileid="2118" duration="200"/>
+   <frame tileid="2119" duration="200"/>
+   <frame tileid="2118" duration="200"/>
+   <frame tileid="2117" duration="200"/>
+   <frame tileid="2116" duration="200"/>
+   <frame tileid="2116" duration="200"/>
+  </animation>
+ </tile>
+ <tile id="2216">
+  <animation>
+   <frame tileid="2216" duration="200"/>
+   <frame tileid="2217" duration="200"/>
+   <frame tileid="2218" duration="200"/>
+   <frame tileid="2219" duration="200"/>
+   <frame tileid="2218" duration="200"/>
+   <frame tileid="2217" duration="200"/>
+   <frame tileid="2216" duration="200"/>
+   <frame tileid="2216" duration="200"/>
+  </animation>
+ </tile>
+</tileset>
